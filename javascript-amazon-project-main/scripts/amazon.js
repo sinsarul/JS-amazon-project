7e6698f1bac06cd385474c028fa5390562313f05
sinsar11456 <sinsarsanu111@gmail.com>
2025-03-13 12:32:02 +0530

    make add to cart intractive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,9 +1,7 @@
-
-
 let productsHTML = "";
 
 products.forEach((product) => {
-    productsHTML  += `
+  productsHTML += `
     <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -48,14 +46,36 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary 
+          js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
         `;
-        
+});
 
-})
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
 
+    let matchingItem;
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+
+    console.log(cart);
+  });
+});
